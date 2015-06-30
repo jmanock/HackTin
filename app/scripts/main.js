279fd592e6657f401b4473cf09c6d129aef72e19
jmanock <jonmanock@gmail.com>
2015-06-30 10:28:16 -0400

    last time

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
-  $.getJSON('scripts/tinder.json', function(data){
+  $.getJSON('scripts/hacks.json', function(data){
     $.each(data, function(key, val){
       // Tinder photo hack
-      // for(var i = 0; i<val.length; i++){
-      //   for(var x = 0; x<val[i].photos.length; x++){
-      //     //$('body').append($('<img src=' + val[i].photos[x].url + '</img>'));
-      //     $('div').append($('<li >' + val[i].name +
-      //     '<img src =' + val[i].photos[x].url + ' height="100" width="100"></img>' + '</li>'));
-      //
-      //   }
-      //   // Other ideas like all the ids, show all the instagram photos
-      // }
-
-      // Instagram hack now
-      for(var i = 0; i< val.length; i++){
-        var insta = val[i].instagram;
-        var nme = val[i].name;
-        if(insta === null || insta === undefined){
-          console.log('nothing here');
-        }else{
-          for(var x = 0; x<insta.photos.length; x++){
-               $('body').append($('<img src=' + insta.photos[x].image + '></img>'));
-              //console.log(insta.photos[x].image);
-          }
+      for(var i = 0; i<val.length; i++){
+        for(var x = 0; x<val[i].photos.length; x++){
+          //$('body').append($('<img src=' + val[i].photos[x].url + '</img>'));
+          $('div').append($('<li >' + val[i].name +
+          '<img src =' + val[i].photos[x].url + ' height="100" width="100"></img>' + '</li>'));
 
         }
+
       }
-    });
-  });
+
+      // Instagram hack
+  //     for(var i = 0; i< val.length; i++){
+  //       var insta = val[i].instagram;
+  //       var nme = val[i].name;
+  //       if(insta === null || insta === undefined){
+  //         console.log('nothing here');
+  //       }else{
+  //         for(var x = 0; x<insta.photos.length; x++){
+  //              $('body').append($('<img src=' + insta.photos[x].image + '></img>'));
+  //             //console.log(insta.photos[x].image);
+  //         }
+  //
+  //       }
+  //     }
+
+
+     });
+   });
 });
